c430c9a2b1a32a7bc8251844a89b0d8577acb90b
Jason Kuhrt <jasonkuhrt@me.com>
2022-02-23 22:21:23 +0000

    format

--- src/Errors/index.ts
+++ src/Errors/index.ts
@@ -3,7 +3,12 @@ import { z } from 'zod'
 /**
  * https://www.prisma.io/docs/reference/api-reference/error-reference
  */
-export const ErrorCodes = z.union([z.literal('P1003'), z.literal('P1000'), z.literal('P2002'), z.literal('P4001')])
+export const ErrorCodes = z.union([
+  z.literal('P1003'),
+  z.literal('P1000'),
+  z.literal('P2002'),
+  z.literal('P4001'),
+])
 
 export type ErrorCodes = z.infer<typeof ErrorCodes>
 
